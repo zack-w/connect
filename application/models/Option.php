604d7e9873487c382bd9478c9e127039ac166445
Zack <zack@exolocity.com>
2014-11-10 16:48:49 -0500

    Added option module

---
+++ application/models/Option.php
@@ -0,0 +1,19 @@
+<?php
+	
+	/* This model will represent a question option in the system. */
+	
+	class Option extends CI_Model {
+		
+		function __construct() {
+			parent::__construct();
+		}
+		
+		function getQuestionOptions( $qid ) {
+			$this->db->where( "QuestionID", $qid );
+			$query = $this->db->get( "cnct_options" );
+			return $query;
+		}
+		
+	}
+
+?>
